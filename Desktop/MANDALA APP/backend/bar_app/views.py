fed0915faf43433f31191a8c66bd375e66f2658f
Blass Simón Muñoz Monterrosa <blasssimom@gmail.com>
2025-10-05 07:55:45 -0500

    implementacion de imagenes y del backend parcialmente de los pedidos, mesas y meseras

--- Desktop/MANDALA APP/backend/bar_app/views.py
+++ Desktop/MANDALA APP/backend/bar_app/views.py
@@ -5,8 +5,14 @@ from .serializers import ProductoSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import MovimientoSerializer
+from .serializers import PedidoSerializer, MesaSerializer, MeseraSerializer
 from .models import Movimiento
+from .models import PedidoProducto, Mesa, Mesera
 from rest_framework import status
+
+class MeseraViewSet(viewsets.ModelViewSet):
+    queryset = Mesera.objects.all().order_by('-id')
+    serializer_class = MeseraSerializer
 
 class ProductoViewSet(viewsets.ModelViewSet):
     queryset = Producto.objects.all().order_by('-id')
@@ -33,6 +39,28 @@ def perform_create(self, serializer):
         }, status=status.HTTP_201_CREATED)
 
 
+class PedidoViewSet(viewsets.ModelViewSet):
+    queryset = Pedido.objects.all().order_by('-fecha_hora')
+    serializer_class = PedidoSerializer
+
+    def perform_create(self, serializer):
+        pedido = serializer.save()
+        print("Pedido creado:", pedido)
+        for item in PedidoProducto.objects.filter(pedido=pedido):
+            producto = item.producto
+            print(producto)
+            if producto.stock < item.cantidad:
+                raise ValueError(f"❌ Stock insuficiente para el producto {producto.nombre}.")
+            producto.stock -= item.cantidad
+            producto.save()
+
+class MesaViewSet(viewsets.ModelViewSet):
+    queryset = Mesa.objects.all().order_by('numero')
+    serializer_class = MesaSerializer
+    def perform_create(self, serializer):
+        if Mesa.objects.filter(numero=serializer.validated_data['numero']).exists():
+            raise ValueError("❌ Ya existe una mesa con ese número.")
+        serializer.save()
 @api_view(['GET'])
 def productos_list(request):
     productos = Producto.objects.all()
